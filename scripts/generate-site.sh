5b4b26df91b376a92207e4cb3a73ba9ac079d51f
Jay Taylor <outtatime@gmail.com>
2019-08-10 16:29:29 +0000

    Sane defaults for generate-site.sh
    - Added new flag to specify output directory.

--- scripts/generate-site.sh
+++ scripts/generate-site.sh
@@ -6,26 +6,31 @@ set -o nounset
 
 
 function showHelp() {
-    echo ".------------------------------------------------------------------------." 1>&2
-    echo "| Static site-generator tool                                             |" 1>&2
-    echo ".---------------.------------+----------------------------.--------------." 1>&2
-    echo "| flag          | description                             | env-var      |" 1>&2
-    echo "+---------------+-----------------------------------------+--------------+" 1>&2
-    echo "| -f             Fast mode - skips content tagging and MD   \$FAST=true|1 |" 1>&2
-    echo "|                rendering (uses / requires stale data)                  |" 1>&2
-    echo "|                                                                        |" 1>&2
-    echo "| -h, ?          This help document                                      |" 1>&2
-    echo "|                                                                        |" 1>&2
-    echo "| -l <limit>     Maximum number of articles to process      \$LIMIT       |" 1>&2
-    echo "|                (default: 500)                                          |" 1>&2
-    echo "|                                                                        |" 1>&2
-    echo "| -o <dir-path>  Huge base directory                        \$HUGO_DIR    |" 1>&2
-    echo "|                                                                        |" 1>&2
-    echo "| -s <dir-path>  JSON data source directory                 \$SRC_DIR     |" 1>&2
-    echo "|                                                                        |" 1>&2
-    echo "| -v             Enable verbose output                      \$VERBOSE     |" 1>&2
-    echo "|                                                                        |" 1>&2
-    echo '`'"-------------'-------------------------------------------'--------------'" 1>&2
+    echo ".-------------------------------------------------------------------------." 1>&2
+    echo "| Static site-generator tool                                              |" 1>&2
+    echo ".---------------.------------------------------------------.--------------." 1>&2
+    echo "| flag          | description                              | env-var      |" 1>&2
+    echo "+---------------+------------------------------------------+--------------+" 1>&2
+    echo "| -b <dir-path>   Huge base directory                        \$HUGO_DIR    |" 1>&2
+    echo "|                 (default: \"quickstart\")                                 |" 1>&2
+    echo "|                                                                         |" 1>&2
+    echo "| -f              Fast mode - skips content tagging and MD   \$FAST=true|1 |" 1>&2
+    echo "|                 rendering (uses / requires stale data)                  |" 1>&2
+    echo "|                                                                         |" 1>&2
+    echo "| -h, ?           This help document                                      |" 1>&2
+    echo "|                                                                         |" 1>&2
+    echo "| -l <limit>      Maximum number of articles to process      \$LIMIT       |" 1>&2
+    echo "|                 (default: 500)                                          |" 1>&2
+    echo "|                                                                         |" 1>&2
+    echo "| -o <dir-path>   Output directory                           \$OUTPUT_DIR  |" 1>&2
+    echo "|                 (default:                                               |" 1>&2
+    echo "|                   /var/www/jaytaylor.com/public_html/hn)                |" 1>&2
+    echo "|                                                                         |" 1>&2
+    echo "| -s <dir-path>   JSON data source directory                 \$SRC_DIR     |" 1>&2
+    echo "|                                                                         |" 1>&2
+    echo "| -v              Enable verbose output                      \$VERBOSE     |" 1>&2
+    echo "|                                                                         |" 1>&2
+    echo '`'"-------------'--------------------------------------------'--------------'" 1>&2
 }
 
 
@@ -37,6 +42,7 @@ function main() {
     local h
     local limit
     local hugoDir
+    local outputDir
     local srcDir
     local verbose
 
@@ -44,13 +50,17 @@ function main() {
 
     fast="${FAST:-}"
     limit="${LIMIT:-500}"
-    hugoDir="${HUGO_DIR:-}"
+    hugoDir="${HUGO_DIR:-quickstart}"
     srcDir="${SRC_DIR:-}"
+    outputDir="${OUTPUT_DIR:-/var/www/jaytaylor.com/hn}"
     verbose="${VERBOSE:-}"
     v=''
 
     while getopts "fh?l:o:s:v" OPT; do
         case "${OPT}" in
+        b)
+            hugoDir="${OPTARG}"
+            ;;
         f)
             fast='true'
             ;;
@@ -62,7 +72,7 @@ function main() {
             limit="${OPTARG}"
             ;;
         o)
-            hugoDir="${OPTARG}"
+            outputDir="${OPTARG}"
             ;;
         s)
             srcDir="${OPTARG}"
@@ -82,14 +92,20 @@ function main() {
 
     echo "DEBUG: $0 Configuration" 1>&2
     echo "DEBUG: $(echo "${0}" | python -c 'import sys; sys.stdout.write("-" * len(sys.stdin.read()))')-------------" 1>&2
-    echo "DEBUG:    fast: ${fast}" 1>&2
-    echo "DEBUG: hugoDir: ${hugoDir}" 1>&2
-    echo "DEBUG:   limit: ${limit}" 1>&2
-    echo "DEBUG:  srcDir: ${srcDir}" 1>&2
+    echo "DEBUG:      fast: ${fast}" 1>&2
+    echo "DEBUG:   hugoDir: ${hugoDir}" 1>&2
+    echo "DEBUG:     limit: ${limit}" 1>&2
+    echo "DEBUG: outputDir: ${outputDir}" 1>&2
+    echo "DEBUG:    srcDir: ${srcDir}" 1>&2
     echo "DEBUG: $(echo "${0}" | python -c 'import sys; sys.stdout.write("-" * len(sys.stdin.read()))')-------------" 1>&2
 
     if [ -z "${hugoDir}" ] ; then
-        echo 'ERROR: missing required parameter: -o <hugo-dir-path>'
+        echo 'ERROR: missing required parameter: -b <hugo-dir-path>'
+        exit 1
+    fi
+
+    if [ -z "${outputDir}" ] ; then
+        echo 'ERROR: missing required parameter: -o <output-dir>'
         exit 1
     fi
 
@@ -114,15 +130,15 @@ function main() {
     #--stepAnalysis
     cd -
 
-    if [ -e /var/www/jaytaylor.com/public_html/hn ] ; then
-        rm -rf /var/www/jaytaylor.com/public_html/hn.old
-        mv /var/www/jaytaylor.com/public_html/hn{,.old}
+    if [ -e "${outputDir}" ] ; then
+        rm -rf "${outputDir}.old"
+        mv "${outputDir}"{,.old}
     fi
 
     find "${hugoDir}/public" -type d -exec chmod 755 {} +
-    mv "${hugoDir}/public" /var/www/jaytaylor.com/public_html/hn
+    mv "${hugoDir}/public" "${outputDir}"
 
-    rm -rf /var/www/jaytaylor.com/public_html/hn.old
+    rm -rf "${outputDir}.old"
 }
 
 if [ "${BASH_SOURCE[0]}" = "${0}" ] ; then
